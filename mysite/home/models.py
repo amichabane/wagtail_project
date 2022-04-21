4fd6cbdede3950b54cbe9c88f774acc2981e613e
Amine chabane <chabane.amine@ofppt-edu.ma>
2022-04-21 11:53:42 +0000

    add carblock

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -6,7 +6,7 @@ from wagtail.core.fields import RichTextField, StreamField
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.blocks import ImageChooserBlock
 
-from website_blocks.blocks import HeroText
+from website_blocks.blocks import HeroText, CardBlock
 
 
 class HomePage(Page):
@@ -15,9 +15,15 @@ class HomePage(Page):
         ('paragraph', blocks.RichTextBlock()),
         ('image', ImageChooserBlock()),
         ('button', blocks.CharBlock()),
-        ('HeroText',HeroText())
+        ('HeroText', HeroText()),
+        ("cards", CardBlock()),
     ])
 
     content_panels = Page.content_panels + [
         StreamFieldPanel('body'),
     ]
+
+
+class Meta:
+    verbose_name = "Home Page"
+    verbose_name_plural = "Home Pages"
--- mysite/website_blocks/blocks.py
+++ mysite/website_blocks/blocks.py
@@ -5,30 +5,28 @@ class HeroText(blocks.StructBlock):
     title = blocks.RichTextBlock(default="Title", required=False)
     description = blocks.RichTextBlock(default="Description", required=False)
     button = blocks.CharBlock(default="Button", required=False)
-    image=ImageChooserBlock(help_text="Dimension mininum :660 x 980px")
+    image = ImageChooserBlock(help_text="Dimension mininum :660 x 980px")
 
-class Meta:
-    template = 'website_blocks/hero_text.html'
-    icon='image'
+    class Meta:
+        template = 'website_blocks/hero_text.html'
+        icon = 'image'
 
 
 class CardBlock(blocks.StructBlock):
-
-    title = blocks.CharBlock(required=True, help_text="Add your title")
+    title = blocks.CharBlock(required=False, help_text="Add your title")
 
     cards = blocks.ListBlock(
         blocks.StructBlock(
             [
-                ("image", ImageChooserBlock(required=True)),
-                ("title", blocks.CharBlock(required=True, max_length=40)),
-                ("text", blocks.TextBlock(required=True, max_length=200)),
+                ("image", ImageChooserBlock(required=False)),
+                ("title", blocks.CharBlock(required=False, max_length=40)),
+                ("text", blocks.TextBlock(required=False, max_length=200)),
                 ("button_page", blocks.PageChooserBlock(required=False)),
-                ("button_url", blocks.URLBlock(required=False, help_text="If the button page above is selected, that will be used first.")),
+                ("button_url", blocks.URLBlock(required=False,
+                                               help_text="If the button page above is selected, that will be used first.")),
             ]
         )
     )
-
     class Meta:  # noqa
         template = "streams/card_block.html"
         icon = "placeholder"
-        label = "Staff Cards"
